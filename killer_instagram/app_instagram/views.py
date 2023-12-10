c02c58a97c250183a4ec4ca58106c67949ebb612
diixo <anatolii.podoba@gmail.com>
2023-12-11 01:59:24 +0200

    Added path, description

--- killer_instagram/app_instagram/views.py
+++ killer_instagram/app_instagram/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Picture
+from .forms import PictureForm
 
 # Create your views here.
 def main(request):
@@ -6,7 +8,8 @@ def main(request):
     return render(request, "app_instagram/index.html", context={"title":"Hello world new instagram"})
 
 def upload(request):
-    return render(request, "app_instagram/upload.html", context={"title":"Hello world from upload"})
+    form = PictureForm(instance=Picture())
+    return render(request, "app_instagram/upload.html", context={"title":"Hello world from upload", "form": form})
 
 def pictures(request):
     return render(request, "app_instagram/pictures.html", context={"title":"Hello world from pictures"})
