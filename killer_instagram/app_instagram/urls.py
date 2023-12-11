ba04fc9e2e57a51d9952a52c82b2a2fd35e2675c
diixo <anatolii.podoba@gmail.com>
2023-12-11 17:04:35 +0200

    Added edit, remove operations

--- killer_instagram/app_instagram/urls.py
+++ killer_instagram/app_instagram/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     #name="root" as in templates
     path("", views.main, name="root"),
     path("upload/", views.upload, name="upload"),
-    path("pictures/", views.pictures, name="pictures"),    
+    path("pictures/", views.pictures, name="pictures"),
+
+    path("pictures/edit/<int:pic_id>", views.edit, name="edit"),
+    path("pictures/remove/<int:pic_id>", views.remove, name="remove"),    
 ]
--- killer_instagram/app_instagram/views.py
+++ killer_instagram/app_instagram/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+import os
+from django.shortcuts import render, redirect
+from django.conf import settings
 from .models import Picture
 from .forms import PictureForm
 
@@ -9,7 +11,33 @@ def main(request):
 
 def upload(request):
     form = PictureForm(instance=Picture())
-    return render(request, "app_instagram/upload.html", context={"title":"Hello world from upload", "form": form})
+    if request.method == "POST":
+        form = PictureForm(request.POST, request.FILES, instance = Picture())
+        if form.is_valid():
+            form.save()
+            return redirect(to="app_instagram:pictures")
+    return render(request, "app_instagram/upload.html", context={"title": "Hello world from upload", "form": form})
 
 def pictures(request):
-    return render(request, "app_instagram/pictures.html", context={"title":"Hello world from pictures"})
+    pictures = Picture.objects.all() #objects as SQL-query
+    return render(request, "app_instagram/pictures.html", 
+                  context={"title": "Hello world from pictures", "pictures": pictures, "media": settings.MEDIA_URL})
+
+def remove(request, pic_id):
+    picture = Picture.objects.filter(pk=pic_id)
+    try:
+        os.unlink(os.path.join(settings.MEDIA_ROOT, str(picture.first().path)))
+    except OSError as e:
+        print(e)
+    picture.delete()
+    return redirect(to="app_instagram:pictures")
+
+def edit(request, pic_id):
+
+    if request.method == "POST":
+        description = request.POST.get('description')
+        Picture.objects.filter(pk=pic_id).update(description=description)
+        return redirect(to="app_instagram:pictures")
+    picture = Picture.objects.filter(pk=pic_id).first()
+    return render(request, "app_instagram/edit.html", 
+                  context={"title": "Hello world from pictures", "pic": picture, "media": settings.MEDIA_URL})
